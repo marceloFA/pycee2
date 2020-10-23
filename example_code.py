bf77facf98dee7f78e15ce197032753203c30710
Marcelo Freitas <jmfda00@gmail.com>
2020-10-23 20:50:11 -0300

    add a default example error and example code

---
+++ example_code.py
@@ -0,0 +1,8 @@
+import collections
+import kivy
+
+# This comment is just used for debugging pycee
+''' This comment is just used for debugging pycee '''
+##
+''' '''
+counter = collections.Counter()
