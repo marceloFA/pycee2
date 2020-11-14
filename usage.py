abc1073a75da8b808ee38c787d33649488a3c887
Marcelo Freitas <jmfda00@gmail.com>
2020-11-13 21:01:11 -0300

    Reflect new changes on usage example

--- usage.py
+++ usage.py
@@ -4,11 +4,11 @@ from pycee.inspection import get_error_info, get_packages
 from pycee.sym_table import get_offending_line
 from pycee.utils import create_argparser
 
-if __name__ == "__main__":
 
+def main():
     args = create_argparser().parse_args()
 
-    error_info = get_error_info(args.file)
+    error_info = get_error_info(args.file_name)
     offending_line = get_offending_line(error_info)
     packages = get_packages(error_info["code"])
     query, pycee_answer, pydoc_answer = handle_error(error_info, offending_line, packages, limit=args.n_answers)
@@ -17,17 +17,21 @@ if __name__ == "__main__":
         print(query)
         exit(1)
 
-    elif query:
+    if query and not args.pycee_answer_only:
         answers = get_answers(query, error_info["traceback"], offending_line)
 
         for i, answer in enumerate(answers):
             print(f"Solution {i}:")
             print(answer)
 
-    elif pycee_answer:
+    if pycee_answer and not args.so_answer_only:
         print(pycee_answer)
 
     # TODO: fix pydocs information, currently not working.
-    elif pydoc_answer:
+    if pydoc_answer:
         print("From PyDocs")
         print("\n".join(pydoc_answer))
+
+
+if __name__ == "__main__":
+    main()
