5ab3cae2b4132bad4168dff060f5992853176ba9
Marcelo Freitas <jmfda00@gmail.com>
2020-12-11 15:04:03 -0300

    Simpler usage example by hidding offending_line into error_info
    TO make the interface of usage simpler, offending_line is now inside error_info and also imported packages information is no longer needed for now and could also be removed

--- usage.py
+++ usage.py
@@ -1,30 +1,25 @@
-from pycee.answers import get_answers
+from pycee.answers import get_so_answers
 from pycee.errors import handle_error
 from pycee.inspection import get_error_info, get_packages
-from pycee.sym_table import get_offending_line
 from pycee.utils import parse_args, remove_cache, print_answers
 
 
 def main():
 
     args = parse_args()
+
     if args.rm_cache:
         remove_cache()
+
     error_info = get_error_info(args.file_name)
-    offending_line = get_offending_line(error_info)
-    packages = get_packages(error_info["code"])
-    query, pycee_answer, pydoc_answer = handle_error(
-        error_info, offending_line, packages, limit=args.n_questions, dry_run=args.dry_run
-    )
-    so_answers, _ = get_answers(
+    query, pycee_hint, pydoc_answer = handle_error(error_info, n_questions=args.n_questions, dry_run=args.dry_run)
+    so_answers, _ = get_so_answers(
         query,
-        error_info["traceback"],
-        offending_line,
-        error_info["message"],
+        error_info,
         cache=args.cache,
         n_answers=args.n_answers,
     )
-    print_answers(so_answers, pycee_answer, pydoc_answer, args)
+    print_answers(so_answers, pycee_hint, pydoc_answer, args)
 
 
 if __name__ == "__main__":
