0059592c7a8fd5a95ab7ab72488fdaa74824d55d
Leonardo Furtado <srleonardofurtado@gmail.com>
2020-12-17 17:51:09 -0300

    remove get_syntax_error_skipable_lines method
    Just another dead code.

--- pycee/sym_table.py
+++ pycee/sym_table.py
@@ -19,30 +19,3 @@ def trim_and_split_code(code: str, error_line: str) -> List[str]:
         trimmed_code = code[0]
 
     return trimmed_code
-
-def get_syntax_error_skipable_lines(traceback):
-    """SyntaxError has as aditional line to exactly where the error is.
-    And this makes the traceback different from other errors.
-    This method will return the number of uninformative lines
-    we can skip from a SyntaxError.
-    """
-    if "^" not in traceback:
-        # accept compiler line
-        return 1
-
-    lines = traceback.split("\n")
-    del lines[3]  # remove inital error message
-    del lines[0]  # remove final error message
-
-    # set max length, if first token ends at the same position as ^
-    # then the error is on the previous line
-    max_length = len(lines[1].split("^")[0]) + 1
-    tmp = re.split(r"[!@#$%^&*_\-+=\(\)\[\]\{\}\\|~`/?.,<>:; ]", lines[0])
-
-    i = 0
-    while not tmp[i]:
-        i += 1
-    temp_len = i + len(tmp[i])
-    # take previous line
-    if temp_len >= max_length:
-        return 2
