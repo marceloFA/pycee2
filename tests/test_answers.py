613b3f4e1315344e845c73321f0ffb19820c5017
Marcelo Freitas <jmfda00@gmail.com>
2020-12-18 20:48:38 -0300

    Add tests for 2 new features
    Test for _ask_google, a method to retrieve stackoverflow questions from googe and one test for a new command line argument to control questions origin (google_questions_only). Also renamed som fixtures but this is not really relevant

--- tests/test_answers.py
+++ tests/test_answers.py
@@ -1,6 +1,8 @@
 from httmock import all_requests, HTTMock
-from pycee.answers import ask_stackoverflow, get_answer_content
-from pycee.utils import ANSWER_URL, Question, Answer
+import googlesearch
+
+from pycee.answers import _ask_stackoverflow, _ask_google, _get_answer_content
+from pycee.utils import Question, Answer
 
 
 # data resources
@@ -14,13 +16,13 @@ questions_data = {
             "is_answered": True,
             "accepted_answer_id": 4,
             "answer_count": 1,
-            "question_id": 1,
+            "question_id": "1",
         },
         {
             "is_answered": False,
             "answer_count": 0,
             "score": 0,
-            "question_id": 2,
+            "question_id": "2",
         },
     ]
 }
@@ -30,7 +32,7 @@ answers_data = {
             "is_accepted": False,
             "score": 20,
             "answer_id": 4,
-            "question_id": 1,
+            "question_id": "1",
             "body": "Body 4",
             "owner": {
                 "display_name": "author 4",
@@ -41,7 +43,7 @@ answers_data = {
             "is_accepted": True,
             "score": 10,
             "answer_id": 3,
-            "question_id": 1,
+            "question_id": "1",
             "body": "Body 3",
             "owner": {
                 "display_name": "author 3",
@@ -51,7 +53,7 @@ answers_data = {
             "is_accepted": False,
             "score": 5,
             "answer_id": 5,
-            "question_id": 1,
+            "question_id": "1",
             "body": "Body 5",
             "owner": {
                 "display_name": "author 5",
@@ -61,21 +63,19 @@ answers_data = {
     ]
 }
 
-# mocks
-
 
 @all_requests
-def question_response(url, request):
+def so_question_response(url, request):
     return {"status_code": 200, "content": questions_data}
 
 
 @all_requests
-def empty_question_response(url, request):
+def empty_so_question_response(url, request):
     return {"status_code": 200, "content": empty_questions}
 
 
 @all_requests
-def answer_response(url, request):
+def answers_response(url, request):
     return {"status_code": 200, "content": answers_data}
 
 
@@ -90,23 +90,44 @@ def empty_answers_response(url, request):
 def test_ask_stackoverflow_skip_unanswered_questions():
 
     question_obj = tuple([Question(id="1", has_accepted=True)])
-    with HTTMock(question_response):
-        questions = ask_stackoverflow(fake_query)
+    with HTTMock(so_question_response):
+        questions = _ask_stackoverflow(fake_query)
     assert questions == question_obj
 
 
 def test_ask_stackoverflow_can_handle_empty_response():
 
-    with HTTMock(empty_question_response):
-        questions = ask_stackoverflow(fake_query)
+    with HTTMock(empty_so_question_response):
+        questions = _ask_stackoverflow(fake_query)
     assert questions == tuple([])
+
+
+def test_ask_google_parses_urls_correctly(monkeypatch):
+
+    questions_from_google = [
+        "https://stackoverflow.com/questions/666666/not-a-real-question",
+        "https://stackoverflow.com/questions/999999/foo-bar-baz",
+    ]
+    expected = tuple(
+        [
+            Question(id="666666", has_accepted=None),
+            Question(id="999999", has_accepted=None),
+        ]
+    )
+
+    def mock_google_search(n_questions):
+        return questions_from_google
+
+    monkeypatch.setattr(googlesearch, "search", mock_google_search)
+    questions = _ask_google("TestExpection: Not a real error :)", n_questions=3)
+    assert questions == expected
 
 
 def test_get_answer_content_from_one_question():
 
-    question_obj = tuple([Question(id=1, has_accepted=True)])
-    with HTTMock(answer_response):
-        answers = get_answer_content(question_obj)
+    question_obj = tuple([Question(id="1", has_accepted=True)])
+    with HTTMock(answers_response):
+        answers = _get_answer_content(question_obj)
 
     expected_answers = [
         Answer(id="4", accepted=False, score=20, body="Body 4", author="author 4", profile_image="foo 4"),
@@ -117,7 +138,7 @@ def test_get_answer_content_from_one_question():
 
 def test_get_answer_content_handle_empty_response():
 
-    question_obj = tuple([Question(id=1, has_accepted=True)])
+    question_obj = tuple([Question(id="1", has_accepted=True)])
     with HTTMock(empty_answers_response):
-        questions = get_answer_content(question_obj)
+        questions = _get_answer_content(question_obj)
     assert questions == tuple([])
